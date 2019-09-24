811c662f90072cc4fbd1840389d4a187f98ac9fd
Taufiq R <taufiq.it2@gmail.com>
2019-09-24 20:45:04 +0700

    first commit

---
+++ src/pbo/pkg10118080/latihan16/penugasan/PBO10118080Latihan16Penugasan.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10118080.latihan16.penugasan;
+
+/**
+ *
+ * @author 
+ * Nama :Taufiq Rizky
+ * NIM  :10118080
+ * Deskripsi Program    : program perhitungan
+ */
+public class PBO10118080Latihan16Penugasan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        
+        System.out.println("Nilai a = "+a);
+        System.out.println("Nilai b = "+b);
+        System.out.println("Nilai c = "+c+"\n");
+        
+        c= a+b;
+        System.out.println("c = a + b = "+c);
+        
+        c+=a;
+        System.out.println("c += a = "+c);
+        
+        c-= a;
+        System.out.println("c -= a = "+c);
+        
+        c*= a;
+        System.out.println("c *= a = "+c);
+        
+        a=10;
+        c=15;
+        
+        c/= a;
+        System.out.println("c /= a = "+c);
+        
+        c%= a;
+        System.out.println("c %= a = "+c);
+        
+        c<<= 2;
+        System.out.println("c <<= 2 = "+c);
+        
+        c>>= a;
+        System.out.println("c >>= 2 = "+c);
+        
+        c>>= a;
+        System.out.println("c >>= a = "+c);
+        
+        c&= a;
+        System.out.println("c &= a = "+c);
+        
+        c^= a;
+        System.out.println("c ^= a = "+c);
+        
+        c|= a;
+        System.out.println("c |= a = "+c);
+    }
+    
+    
+}
